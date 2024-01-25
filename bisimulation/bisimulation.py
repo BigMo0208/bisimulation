8463ffb34aeec16c4b44f6b4377afb5287ebdc82
BigMo0208 <124939606+BigMo0208@users.noreply.github.com>
2024-01-25 22:25:58 +0100

    bisim_eq.classes.ipynb

--- bisimulation/bisimulation.py
+++ bisimulation/bisimulation.py
@@ -1,71 +1,197 @@
+import itertools
+
+import numpy as np
+
+import pandas as pd
+
+import seaborn as sns
+
+import matplotlib.pyplot as plt
+
+import seaborn as sns
+
+import networkx as nx
+
 import rustworkx as rx
 
-def ueberpruefe_uebergaenge(graph, start, ziel, bisimulationsrelation):
-    for nachbar in graph.neighbors(start):
-        bisimilar_found = False
-        for ziel_nachbar in graph.neighbors(ziel):
-            if (nachbar, ziel_nachbar) in bisimulationsrelation or nachbar == ziel_nachbar:
-                bisimilar_found = True
-                break
-        if not bisimilar_found:
-            return False
-    return True
 
-def ist_bisimilar(graph, node1, node2, bisimulationsrelation):
-    if (node1, node2) in bisimulationsrelation or (node2, node1) in bisimulationsrelation:
-        return True
 
-    if not ueberpruefe_uebergaenge(graph, node1, node2, bisimulationsrelation):
-        return False
+from bispy import compute_maximum_bisimulation, Algorithms
+from llist import dllist
+from rustworkx.visualization import mpl_draw
+from bispy.saha.saha import saha as saha_algorithm
+from bispy.utilities.graph_normalization import (
+    check_normal_integer_graph,
+    convert_to_integer_graph,
+    back_to_original,
+)
+from bispy.utilities.graph_decorator import decorate_nx_graph, to_tuple_list
+from bispy.paige_tarjan.paige_tarjan import paige_tarjan_qblocks
 
-    if not ueberpruefe_uebergaenge(graph, node2, node1, bisimulationsrelation):
-        return False
+from typing import Union, List, Dict, Any, Tuple
+from bispy.utilities.graph_entities import _QBlock, _Vertex
 
-    bisimulationsrelation.add((node1, node2))
-    bisimulationsrelation.add((node2, node1))
-    return True
 
-def finde_bisimulationsrelation(graph):
-    bisimulationsrelation = set()
 
-    for node1 in graph.node_indices():
-        for node2 in graph.node_indices():
-            if node1 != node2:
-                ist_bisimilar(graph, node1, node2, bisimulationsrelation)
 
-    return bisimulationsrelation
+from typing import Dict, Tuple, Any, List
 
-def erstelle_quotientengraph(original_graph, bisimulationsrelation):
-    quotienten_graph = rx.PyDiGraph()
-    gruppen_zu_knoten = {}
+from bispy.saha.saha_partition import saha_algorithm
 
-    for node in original_graph.node_indices():
-        gruppe = frozenset({n for n, m in bisimulationsrelation if n == node or m == node})
-        if not gruppe:
-            gruppe = frozenset({node})
-        if gruppe not in gruppen_zu_knoten:
-            neuer_knoten = quotienten_graph.add_node(gruppe)
-            gruppen_zu_knoten[gruppe] = neuer_knoten
+from typing import Iterable, List, Tuple, Dict, Union
+from itertools import islice
+from llist import dllist
+from bispy.utilities.graph_entities import _QBlock as _Block, _Vertex, _XBlock
+from bispy.utilities.graph_decorator import decorate_nx_graph
+from bispy.paige_tarjan.paige_tarjan import paige_tarjan_qblocks
+from bispy.utilities.graph_normalization import (
+    check_normal_integer_graph,
+    convert_to_integer_graph,
+    back_to_original,
+)
+from bispy.utilities.graph_entities import _XBlock
+from bispy.dovier_piazza_policriti.ranked_partition import RankedPartition
 
-    for node1 in original_graph.node_indices():
-        gruppe1 = frozenset({n for n, m in bisimulationsrelation if n == node1 or m == node1})
-        for node2 in original_graph.neighbors(node1):
-            gruppe2 = frozenset({n for n, m in bisimulationsrelation if n == node2 or m == node2})
-            if gruppe1 != gruppe2:
-                quotienten_graph.add_edge(gruppen_zu_knoten[gruppe1], gruppen_zu_knoten[gruppe2], None)
 
-    return quotienten_graph
+from llist import dllist, dllistnode
+from typing import List, Dict, Any, Tuple, Iterable
+import networkx as nx
 
-# Erstellen und Befüllen des Originalgraphen
-original_graph = rx.PyDiGraph()
-node_a = original_graph.add_node("A")
-node_b = original_graph.add_node("B")
-original_graph.add_edge(node_a, node_b, None)  # Kante von A nach B
+from bispy.utilities.graph_entities import (
+    _Vertex,
+    _XBlock,
+    _QBlock,
+    _Count,
+)
+from bispy.paige_tarjan.compound_xblocks_container import (
+    CompoundXBlocksContainer,
+)
+from bispy.utilities.graph_decorator import (
+    decorate_nx_graph,
+    preprocess_initial_partition,
+    to_tuple_list,
+)
+from bispy.utilities.graph_normalization import (
+    check_normal_integer_graph,
+    convert_to_integer_graph,
+    back_to_original,
+)
+# Erstellen der zufälligen Di.Graphen
 
-# Finden der Bisimulationsrelation und Erstellen des Quotientengraphen
-bisimulationsrelation = finde_bisimulationsrelation(original_graph)
-quotienten_graph = erstelle_quotientengraph(original_graph, bisimulationsrelation)
+def generate_random_digraph(node_counts: list[int], sparsity: list[float], one_divided_samplerate: int):
+    # Generate a random binary adjacency matrix for a digraph with the given sparsity
+    adj_matrix_factory = lambda n, s, samples: np.random.choice([0, 1], size=(n, n, samples), p=[s, 1 - s])
+    graph_factory = lambda adj_matrix: nx.from_numpy_array(adj_matrix, create_using = nx.DiGraph)
+    
+    possible_graphs = [adj_matrix_factory(n, s, n**2//one_divided_samplerate) for n, s in itertools.product(node_counts, sparsity)]
+    graphs = [[graph_factory(adj_matrix[:,:,i]) for i in range(adj_matrix.shape[2])] for adj_matrix in possible_graphs]
+    
+    return graphs
 
-# Ausgabe der Anzahl der Knoten und Kanten im Quotientengraphen
-print("Anzahl der Knoten im Quotientengraphen:", quotienten_graph.num_nodes())
-print("Anzahl der Kanten im Quotientengraphen:", quotienten_graph.num_edges())
+
+# Beispiel: Verwendung der Funktion
+node_counts = [10, 20]  # Beispiel: Anzahl der Knoten
+sparsity = [0.2, 0.5]   # Beispiel: Dichte
+one_divided_samplerate = 10  # Beispiel: Stichprobengröße
+
+# Generieren von Graphen
+generated_graphs = generate_random_digraph(node_counts, sparsity, one_divided_samplerate)
+
+
+# Bestimmen der größten Bisimulation
+for graph_list in generated_graphs:
+    for graph in graph_list:
+        bisimulation_result = compute_maximum_bisimulation(graph)
+def graph_factory(adj_matrix):
+    return nx.from_numpy_array(adj_matrix, create_using=nx.DiGraph)
+print(results_df.head())
+# Korrelation zwischen Parametern berechnen
+corr_mat = results_df.corr().stack().reset_index(name="correlation")
+
+# Visualisierung der Korrelationsmatrix
+sns.set_theme(style="whitegrid")
+g = sns.relplot(
+    data=corr_mat,
+    x="level_0", y="level_1", hue="correlation", size="correlation",
+    palette="vlag", hue_norm=(-1, 1), edgecolor=".7",
+    height=10, sizes=(50, 250), size_norm=(-.2, .8),
+)
+
+# Einstellen des Diagramms
+g.set(xlabel="", ylabel="", aspect="equal")
+g.despine(left=True, bottom=True)
+g.ax.margins(.02)
+for label in g.ax.get_xticklabels():
+    label.set_rotation(90)
+# Erstellen der Korrelationsmatrix
+
+# Erstellen eines leeren DataFrames
+results_df = pd.DataFrame(columns=["Node_Count", "Sparsity", "Bisimulation_Size"])
+
+# Durchlaufen der generierten Graphen und Berechnung der Bisimulation
+for node_count_idx, node_count in enumerate(node_counts):
+    for sparsity_idx, sparsity_value in enumerate(sparsity):
+        for graph in generated_graphs[node_count_idx][sparsity_idx]:
+            # Überprüfen Sie, ob der Graph ein nx.DiGraph ist
+            if isinstance(graph, nx.DiGraph):
+                try:
+                    bisimulation_result = compute_maximum_bisimulation(graph)
+                    bisimulation_size = len(bisimulation_result)  # Größe der Äquivalenzklassen
+                    results_df = results_df.append({"Node_Count": node_count, "Sparsity": sparsity_value, "Bisimulation_Size": bisimulation_size}, ignore_index=True)
+                except Exception as e:
+                    print(f"Ein Fehler ist aufgetreten: {e}")
+            else:
+                print("Der Graph ist nicht vom Typ nx.DiGraph")
+
+
+# Weiterverarbeitung des DataFrames für die Darstellung
+
+corr_mat = results_df.corr().stack().reset_index(name="correlation")
+
+# Visualisierung der Korrelationsmatrix
+sns.set_theme(style="whitegrid")
+g = sns.relplot(
+    data=corr_mat,
+    x="level_0", y="level_1", hue="correlation", size="correlation",
+    palette="vlag", hue_norm=(-1, 1), edgecolor=".7",
+    height=10, sizes=(50, 250), size_norm=(-.2, .8),
+)
+
+# Tweak the figure to finalize
+g.set(xlabel="", ylabel="", aspect="equal")
+g.despine(left=True, bottom=True)
+g.ax.margins(.02)
+for label in g.ax.get_xticklabels():
+    label.set_rotation(90)
+
+
+sns.set_theme(style="whitegrid")
+
+# Load the brain networks dataset, select subset, and collapse the multi-index
+df = compute_maximum_bisimulation
+
+used_networks = [generated_graphs]
+
+used_columns = (df.columns.get_level_values("network").astype(int).isin(used_networks))
+
+df = df.loc[:, used_columns]
+
+df.columns = df.columns.map("-".join)
+
+# Compute a correlation matrix and convert to long-form
+corr_mat = df.corr().stack().reset_index(name="correlation")
+
+# Draw each cell as a scatter point with varying size and color
+g = sns.relplot(
+    data=corr_mat,
+    x="level_0", y="level_1", hue="correlation", size="correlation",
+    palette="vlag", hue_norm=(-1, 1), edgecolor=".7",
+    height=10, sizes=(50, 250), size_norm=(-.2, .8),
+)
+
+# Tweak the figure to finalize
+g.set(xlabel="", ylabel="", aspect="equal")
+g.despine(left=True, bottom=True)
+g.ax.margins(.02)
+for label in g.ax.get_xticklabels():
+    label.set_rotation(90)
